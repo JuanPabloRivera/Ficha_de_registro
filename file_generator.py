e1a5a3bd6976bf3a987737b7099547f23beb3fb1
Juan Pablo Rivera Ruiz <jprr.2000@hotmail.com>
2021-05-05 13:20:08 -0500

    txt and docx files creation functionality added

--- file_generator.py
+++ file_generator.py
@@ -4,7 +4,9 @@ from reportlab.lib.utils import ImageReader
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.colors import Color
 from reportlab.lib.units import inch
-#from docx import Document
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Pt
 
 class FileGenerator:
     def createPDF(id, forename, surname, category):
@@ -30,14 +32,37 @@ class FileGenerator:
         canvas.drawCentredString(width/2, 5.5*inch, f'{forename} {surname}')
         canvas.setFont('Helvetica', 20)
         canvas.drawCentredString(width/2, 4*inch, f"Por su participación en la categoría {category}")
-        canvas.drawCentredString(width/2, 3.5*inch, ' en la copa de programación competitiva')
+        canvas.drawCentredString(width/2, 3.5*inch, 'en la copa de programación competitiva')
         canvas.drawCentredString(width/2, 3*inch, 'Guadalajara 2021.')
         
         #Closing file
         canvas.save()
 
     def createTXT(id, forename, surname, category):
-        pass
+        f = open(f"reconocimiento-{id}.txt", 'w')
+        f.write(f"Torneo de Programación Competitiva\nCopa Guadalajara 2021\n\nSe otorga el reconocimiento a:\n{forename} {surname}")
+        f.write(f'\n\nPor su participación en la categoría {category}\nen la copa de programación competitiva\nGuadalajara 2021.')
+        f.close()
 
     def createDOCX(id, forename, surname, category):
-        pass
+        f = Document()
+
+        style = f.styles['Normal']
+        font = style.font
+        font.name = 'Helvetica'
+        font.size = Pt(16)
+
+        heading = f.add_heading("Torneo de Programación Competitiva\nCopa Guadalajara 2021", 0)
+        heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+        paragraph1 = f.add_paragraph(f"\n\n\nSe otorga el reconocimiento a:\n")
+        paragraph1.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        runner = paragraph1.add_run(f'{forename} {surname}')
+        runner.bold = True
+
+        paragraph2 = f.add_paragraph(f'\n\nPor su participación en la categoría {category}\nen la copa de programación competitiva\nGuadalajara 2021.')
+        paragraph2.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+        f.save(f"reconocimiento-{id}.docx")
+
+    createDOCX(1, 'Juanito', 'Perez', 'avanzado')
